64799b9ed3bc4c2547371451625168b8845f7682
Andrew Chan <andrew.koonkit.chan@gmail.com>
2020-03-24 14:13:21 +0000

    try migrate

--- QueryTest/Model.cs
+++ QueryTest/Model.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
@@ -11,37 +13,37 @@ namespace AsyncDemo
         public virtual DbSet<Blog> Blogs { get; set; }
         public virtual DbSet<Post> Posts { get; set; }
 
-        public static bool _created = false;
-        public static readonly ILoggerFactory DbCommandConsoleLoggerFactory = LoggerFactory.Create(builder => {
+        public static readonly ILoggerFactory DbCommandConsoleLoggerFactory = LoggerFactory.Create(builder =>
+        {
             builder.AddFilter("Microsoft", LogLevel.Information)
-                   .AddFilter("System", LogLevel.Information)
-                   .AddFilter("AsyncDemo.Program", LogLevel.Debug)
-                   .AddConsole();
+                .AddFilter("System", LogLevel.Information)
+                .AddFilter("AsyncDemo.Program", LogLevel.Debug)
+                .AddConsole();
         });
+
         public BloggingContext()
         {
-            if (!_created)
-            {
-                _created = true;
-                Database.EnsureDeleted();
-                Database.EnsureCreated();
-            }
+            Database.Migrate();
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=BlogsDB;Username=postgres;Password=pitest;Pooling=true;MinPoolSize=0;MaxPoolSize=50");
+                optionsBuilder.UseNpgsql(
+                    "Host=localhost;Port=5432;Database=BlogsDB;Username=postgres;Password=pitest;Pooling=true;MinPoolSize=0;MaxPoolSize=50");
             }
-            optionsBuilder.UseLoggerFactory(DbCommandConsoleLoggerFactory);
 
+            optionsBuilder.UseLoggerFactory(DbCommandConsoleLoggerFactory);
         }
     }
 
     public class Blog
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int BlogId { get; set; }
+
         public string Name { get; set; }
 
         public virtual List<Post> Posts { get; set; }
@@ -49,7 +51,10 @@ namespace AsyncDemo
 
     public class Post
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int PostId { get; set; }
+
         public string Title { get; set; }
         public string Content { get; set; }
 
